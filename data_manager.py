f66d0e4db6d139d9734752eeb307c81d4d9e46b1
Hector DelaRosa <hdelaros@gmail.com>
2023-03-23 14:17:17 -0400

    fetch_flight_data method added

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,18 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import os
+SHEETY_ENDPOINT = os.environ['SHEETY_ENDPOINT']
+BASIC_AUTH = HTTPBasicAuth(os.environ['AUTH_USERNAME'], os.environ['AUTH_PASS'])
+
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.flight_data_list = None
+
+    def fetch_flight_data(self):
+        response = requests.get(SHEETY_ENDPOINT, auth=BASIC_AUTH)
+        data = response.json()
+        self.flight_data_list = data['prices']
+
+        return self.flight_data_list
